1f4c5c6f8257c2628c58c1afca6d0a9813811aa4
Nino Faraguna <nino.faraguna@playhera.com>
2023-12-05 20:30:35 +0100

    hh

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   output: "export",
   distDir: "dist",
-  async headers() {
-    return [
-      {
-        // Cache static assets for a year (adjust the maxAge value as needed)
-        source: "/_next/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
